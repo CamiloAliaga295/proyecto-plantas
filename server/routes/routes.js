e4d88388b79d06d2b07be6101d066b68d2442bb2
Ana Belen <abilabae@gmail.com>
2023-08-08 20:39:09 -0400

    creacion de carpetar y levantamiento del servidor

---
+++ server/routes/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+const router = express.Router();
+
+router.get('/', (req,res)=>{
+    res.send('Servidor en express')
+})
+
+module.exports= router;
